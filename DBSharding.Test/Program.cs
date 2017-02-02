ca9f4c1ce6e8877dccb4f84ba15c1f5acc3df9b9
Robin <zhao_yabin@sohu.com>
2017-02-03 02:34:30 +0800

    修改索引

--- DBSharding.Test/Program.cs
+++ DBSharding.Test/Program.cs
@@ -15,14 +15,14 @@ namespace DBSharding.Test
         static void Main(string[] args)
         {
             //注册数据库连接
+            IDbConnection db0 = new MySqlConnection(ConfigurationManager.ConnectionStrings["db0"].ConnectionString);
             IDbConnection db1 = new MySqlConnection(ConfigurationManager.ConnectionStrings["db1"].ConnectionString);
             IDbConnection db2 = new MySqlConnection(ConfigurationManager.ConnectionStrings["db2"].ConnectionString);
-            IDbConnection db3 = new MySqlConnection(ConfigurationManager.ConnectionStrings["db3"].ConnectionString);
 
             Dictionary<string, IDbConnection> connectionDic = new Dictionary<string, IDbConnection>();
-            connectionDic.Add("0", db1);
-            connectionDic.Add("1", db2);
-            connectionDic.Add("2", db3);
+            connectionDic.Add("0", db0);
+            connectionDic.Add("1", db1);
+            connectionDic.Add("2", db2);
 
             ShardingConnUtils.RegisConnGroup(connectionDic);
 
